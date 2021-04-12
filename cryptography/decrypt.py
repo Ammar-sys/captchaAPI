71601a5f223d513af17980adbc9ba3ce710ae809
ammarsys <69536330+ammarsys@users.noreply.github.com>
2021-04-12 19:13:32 +0200

    Update decrypt.py

--- cryptography/decrypt.py
+++ cryptography/decrypt.py
@@ -3,3 +3,5 @@ import ...
 
 def decrypt(string: str) -> str:
     # Good luck!
+    # In case you are testing a clone of my app, I will provide an simple example of a decrypt function.
+    return string[::-1]

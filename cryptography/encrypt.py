88d1a1c6a6c1dd03251013b99b920ad776136750
ammarsys <69536330+ammarsys@users.noreply.github.com>
2021-05-05 18:12:05 +0200

    Delete encrypt.py

--- cryptography/encrypt.py
+++
@@ -1,6 +0,0 @@
-import ...
-
-def encrypt(string: str) -> str:
-    # Good luck!
-    # In case you are testing a clone of my app, I will provide an simple example of a decrypt function.
-    return string[::-1]
